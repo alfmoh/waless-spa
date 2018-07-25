07c290668e12ffb70f86fd40115ab49125bf7d20
alf <alfmohenu@outlook.com>
2018-07-25 12:31:08 +0300

    http get test to check SPA and API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  values;
+
+  constructor(private http:HttpClient) {
+    this.values = http.get("http://localhost:5000/api/values");
+  }
 }
