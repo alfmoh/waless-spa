68bcb7439a452799943b800d80898851a0d97b40
alf <alfmohenu@outlook.com>
2018-08-14 18:39:31 +0300

    Site title change according to track implemented.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { BrowserModule } from "@angular/platform-browser";
+import { BrowserModule, Title } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
@@ -18,7 +18,7 @@ import { SharedModule } from "./shared/shared.module";
     SharedModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [Title],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/shared/helpers/playerhandler.ts
+++ src/app/shared/helpers/playerhandler.ts
@@ -1,9 +1,11 @@
-import { BehaviorSubject, forkJoin } from 'rxjs';
+import { BehaviorSubject, forkJoin } from "rxjs";
 import { PlayerService } from "./../../core/services/player.service";
 import { Injectable } from "@angular/core";
 import { DeezerService } from "../services/deezer.service";
 import { Track } from "../models/Track";
 import { sampleTracks } from "../temp/_lorem";
+import { take, pairwise } from "rxjs/operators";
+import { Title } from "@angular/platform-browser";
 
 @Injectable({
   providedIn: "root"
@@ -12,19 +14,29 @@ export class PlayerHanlder {
   isPlaying: boolean = false;
   index: number;
   tracks$ = new BehaviorSubject<any>(sampleTracks);
+  siteTitle;
 
   constructor(
     private playerService: PlayerService,
-    private deezer: DeezerService
+    private deezer: DeezerService,
+    private titleService: Title
   ) { }
 
   initTracks(tracks: Track[]): void {
-
     let obsArr = tracks.map(track => this.deezer.getTrack(track.id));
     forkJoin(...obsArr).subscribe(trackArr => {
       this.tracks$.next(trackArr);
-    })
-
+    });
+    this.tracks$
+      .pipe(
+        take(2),
+        pairwise()
+      )
+      .subscribe(t => {
+        this.siteTitle = t[1][this.playerService.index].title_short
+        this.titleService.setTitle(this.siteTitle)
+      }
+      );
     this.playerService.init(tracks);
   }
 
@@ -43,6 +55,11 @@ export class PlayerHanlder {
 
   next() {
     this.stop();
+    this.tracks$.subscribe(t => {
+      this.siteTitle = t[this.playerService.index + 1].title_short
+      this.titleService.setTitle(this.siteTitle)
+    }
+    );
     this.playerService.playNext();
   }
 
@@ -61,15 +78,14 @@ export class PlayerHanlder {
 
   start(album) {
     if (this.isPlaying) this.stop();
-    this.deezer.getTrackList(album.tracklist)
-      .subscribe((tracks: Track[]) => {
-        this.initTracks(tracks);
-        this.playerService.index = 0;
-        this.play();
-      });
+    this.deezer.getTrackList(album.tracklist).subscribe((tracks: Track[]) => {
+      this.initTracks(tracks);
+      this.playerService.index = 0;
+      this.play();
+    });
   }
 
-  startSelectedTrack(tracks,trackIndex) {
+  startSelectedTrack(tracks, trackIndex) {
     if (this.isPlaying) this.stop();
 
     this.initTracks(tracks);
