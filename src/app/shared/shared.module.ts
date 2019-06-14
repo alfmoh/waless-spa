cba33445960f38c8d453f19fa8757f7cf576b555
alfred <alfmohenu@gmail.com>
2019-06-14 08:25:35 +0000

    Bug fix - Component not rendering when params change

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@ import { StoreModule } from "@ngrx/store";
 import { CustomSlice } from "./pipes/customslice.pipe";
 import { HttpClientJsonpModule } from "@angular/common/http";
 import { TrackDurationPipe } from "./pipes/TrackDuration.pipe";
-import { RouterModule } from "@angular/router";
+import { RouterModule, RouteReuseStrategy } from "@angular/router";
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
@@ -11,6 +11,7 @@ import { LeftSidebarComponent } from "./components/left-sidebar/left-sidebar.com
 import { PlayerComponent } from "./components/player/player.component";
 import { CurrentlyPlayingComponent } from "./components/currently-playing/currently-playing.component";
 import { sharedModuleFeature } from "./helpers/constants";
+import { CustomRouteReuseStrategy } from "./helpers/customRouteReuseStrategy";
 import { sharedReducer } from "./state/shared.reducer";
 
 @NgModule({
@@ -37,6 +38,9 @@ import { sharedReducer } from "./state/shared.reducer";
     TrackDurationPipe,
     CustomSlice,
     CurrentlyPlayingComponent
+  ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: CustomRouteReuseStrategy }
   ]
 })
 export class SharedModule {}
