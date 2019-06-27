3978c9700db4bf484e0f85cf4efe33b60bfdc7e0
alfred <alfmohenu@gmail.com>
2019-06-27 05:58:17 +0000

    Track duration implementation updated. Last updated added to playlist No playlist message added.

--- src/app/shared/pipes/TrackDuration.pipe.ts
+++ src/app/shared/pipes/TrackDuration.pipe.ts
@@ -4,24 +4,40 @@ import { Pipe, PipeTransform } from "@angular/core";
   name: "trackDuration"
 })
 export class TrackDurationPipe implements PipeTransform {
-  transform(value: number): any {
+  transform(value: number, prependMins: boolean): any {
     if (value <= 60) return this.dotToColon(value);
-    else if (value <= 3600) return this.toMinutes(value);
+    else if (value <= 3600) return this.toMinutes(value, prependMins);
     else if (value > 3600) return this.toHours(value);
   }
 
-  private toMinutes(seconds) {
+  private toMinutes(seconds: number, prependMins: boolean) {
     const value = seconds / 60;
-    const valStr = value.toString();
-    if (valStr.length < 2) return `${valStr}:00`;
-    return this.dotToColon(Math.round((value + 0.00001) * 100) / 100);
+    const result = this.format(value);
+    if (value.toString().length < 2) return `${value}:00`;
+    const colonValue = this.dotToColon(result);
+    const valueBeforeColon = colonValue.slice(0, colonValue.indexOf(":"));
+    const valueAfterColon = colonValue.slice(
+      colonValue.indexOf(":") + 1,
+      colonValue.length
+    );
+    if (valueAfterColon.length === 1) {
+      if (prependMins)
+        return +valueAfterColon === 1
+          ? `${valueBeforeColon} minutes : 0${valueAfterColon}second`
+          : `${valueBeforeColon} minutes : 0${valueAfterColon}seconds`;
+      return `${valueBeforeColon}:0${valueAfterColon}`;
+    }
+    return prependMins
+      ? `${valueBeforeColon} minutes : ${valueAfterColon}seconds`
+      : colonValue;
   }
 
-  private toHours(seconds) {
+  private toHours(seconds: number) {
     const value = seconds / 3600;
     const valStr = value.toString();
     if (valStr.length < 2) return valStr + " hours";
-    const numColon = this.dotToColon(Math.round((value + 0.00001) * 100) / 100);
+    const result = this.format(value);
+    const numColon = this.dotToColon(result);
     const strArr = numColon.split("");
     const colonIndex = strArr.indexOf(":");
     +strArr[colonIndex - 1] > 1
@@ -32,9 +48,27 @@ export class TrackDurationPipe implements PipeTransform {
     return strArr.join("");
   }
 
-  private dotToColon(value): string {
+  private format(value: number) {
+    const roundUp = Math.round((value + 0.00001) * 100) / 100;
+    const strRoundUp = roundUp.toString();
+    const dotIndex = strRoundUp.indexOf(".");
+    let wholeNum = +strRoundUp.slice(0, dotIndex);
+    let decimalNum = +strRoundUp.slice(dotIndex + 1, strRoundUp.length);
+    while (decimalNum > 59) {
+      wholeNum++;
+      decimalNum = this.modulus60(decimalNum);
+    }
+    const result = `${wholeNum}.${decimalNum}`;
+    return result;
+  }
+
+  private dotToColon(value: string | number): string {
     const valueToString = value.toString();
     if (valueToString.includes(".")) return valueToString.replace(".", ":");
     return valueToString;
+  }
+
+  private modulus60(value: number) {
+    return value % 60;
   }
 }
