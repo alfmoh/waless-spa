f223ceca21431b7f6a9ffe8e65157862282d6bc8
alfred <alfmohenu@gmail.com>
2019-07-18 14:42:48 +0000

    Share functionality implemented.

---
+++ src/app/shared/directives/copy.directive.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { CopyDirective } from './copy.directive';
+
+describe('Directive: Copy', () => {
+  it('should create an instance', () => {
+    const directive = new CopyDirective();
+    expect(directive).toBeTruthy();
+  });
+});
